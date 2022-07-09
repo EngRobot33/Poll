43e928609923d9f4bda85be277056022707a5d82
EngRobot33 <hmdkhsrvee@gmail.com>
2022-07-09 21:13:50 +0430

    Add Question and Choice models and admins

--- poll/admin.py
+++ poll/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+  
+admin.site.site_header = "MaHa Admin"
+admin.site.site_title = "MaHa Admin Area"
+admin.site.index_title = "Welcome to the MaHa Admin Area"
+  
+  
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 4
+  
+  
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}), ('Date Information', {'fields': ['published_date'], 'classes': ['collapse']}),]
+    inlines = [ChoiceInLine]
+  
+  
+admin.site.register(Question, QuestionAdmin)
--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=256)
+    published_date = models.DateTimeField(default=timezone.now,)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    choice_text = models.CharField(max_length = 256)
+    votes = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.choice_text
